19ea246aa5c4101cc2bb4a9394eeb6379859f75d
Tomas Mikeska <tomikeska@gmail.com>
2020-11-27 12:55:11 +0100

    extract landmarks script - add restart from index option

--- scripts/extract_landmarks.py
+++ scripts/extract_landmarks.py
@@ -13,11 +13,13 @@ from utils import mkdir, file_listing, dir_listing, get_file_name
 fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, face_detector='blazeface')
 
 
-def extract_landmarks(dataset_path='data/grid/'):
+def extract_landmarks(dataset_path='data/grid/', initial_index=0):
     mkdir(dataset_path + '/landmarks')
     filepaths = [f for d in dir_listing(dataset_path + '/video/') for f in file_listing(d, extension='mpg')]
+    filepaths_subset = filepaths[initial_index:]
 
-    for video_path in tqdm(filepaths):
+    for video_path in tqdm(filepaths_subset, initial=initial_index, total=len(filepaths)):
+        print(video_path)
         frames = skvideo.io.vread(video_path).transpose(0, 3, 1, 2)
         video_tensor = torch.Tensor(frames)
         landmarks = fa.get_landmarks_from_batch(video_tensor.cuda())
